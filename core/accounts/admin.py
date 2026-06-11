5363d25882f69d908ea2eab5435d5b51f41b3724
pedimmdi <pedimmdi@gmail.com>
2026-06-11 23:23:14 +0330

    feat(accounts): add Profile model to models.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Profile
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("GroupPermissions", {"fields": ("groups", "user_permissions")})
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_active", "is_staff")}
+        ),
+    )
+    search_fields = ["email"]
+    ordering = ["email"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -39,3 +41,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.email
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.user.email
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
